da14f049ccf42e725c688f74a954348426796b7b
Ezequiel Bertti <ebertti@gmail.com>
2016-11-18 22:19:46 -0200

    gfd

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,59 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function pronto() {
+  var $btn = document.getElementById('salvar_btn');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  $btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
 
-        console.log('Received Event: ' + id);
-    }
-};
+    var $input = document.getElementById('info_id');
+    var texto = $input.value;
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+      fs.root.getFile('meu.txt', {create: true, exclusive:false}, function (fe) {
+        escrever_arquivo(fe, texto);
+      })
+    }, deu_erro);
+
+  });
+
+  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+    fs.root.getFile('meu.txt', {create: false, exclusive:false}, function (fe) {
+      ler_arquivo(fe);
+    })
+  });
+}
+
+function ler_arquivo(fe) {
+  fe.file(function (arquivo) {
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+      console.log("Successful file read: " + this.result);
+    };
+    console.log(arquivo);
+    reader.readAsText(arquivo);
+  })
+}
+
+function escrever_arquivo(fileEntry, conteudo) {
+  fileEntry.createWriter(function (writer) {
+    writer.onwriteend = function() {
+      console.log("Successful file write...");
+    };
+    writer.onerror = function (e) {
+      console.log("Failed file write: " + e.toString());
+    };
+    writer.write(conteudo);
+  })
+}
+
+document.addEventListener('deviceready', pronto);
+
+function deu_erro(v) {
+  console.log('deu erro', v);
+}
+
+//leo x3
+//lucio x2
+//romario
+//diogo
+//yuri
