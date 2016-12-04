66d648cbce23dcaf5b2eb000e0a02fb7640eb0fb
Ezequiel Bertti <ebertti@gmail.com>
2016-12-04 16:18:33 -0200

    exemplo do facebook para phonegap

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,30 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function pronto() {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    var $logar = document.getElementById('logar');
+    var $dados = document.getElementById('dados');
 
-        console.log('Received Event: ' + id);
-    }
-};
+    $logar.addEventListener('click', function (evt) {
+        evt.preventDefault();
+
+        $dados.innerHTML = 'clicou';
+        facebookConnectPlugin.login(['public_profile','email'], function(sucesso){
+
+            $dados.innerHTML = 'fez login';
+            var facebook_id = sucesso.authResponse.userID;
+
+            localStorage.setItem('facebook_id', facebook_id);
+            $dados.innerHTML = facebook_id;
+
+            facebookConnectPlugin.api('me', ['public_profile'], function(dados){
+                localStorage.setItem('nome', dados.name);
+                $dados.innerHTML = JSON.stringify(dados);
+            }, function (error){
+                console.log('deu errado na api' + erro);
+            })
+        }, function(erro) {
+            console.log('deu errado no login' + erro);
+        });
+    })
+}
+
+document.addEventListener('deviceready', pronto);
